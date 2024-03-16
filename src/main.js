1ba46e6c95b776fe77bd2769db3941bd74ba6c80
Ngô Văn Vụ <ngovanvu203@gmail.com>
2024-03-16 07:01:44 +0700

    products-page

--- src/main.js
+++ src/main.js
@@ -1,129 +1,162 @@
 $(jQuery).ready(function () {
-    if ($(".collection-swiper").length > 0) {
-        new Swiper(".collection-swiper", {
-            slidesPerView: 3,
-            spaceBetween: 0,
-            preventInteractionOnTransition: false,
-            breakpoints: {
-                320: {
-                    slidesPerView: 1,
-                    spaceBetween: 0,
-                },
-                768: {
-                    slidesPerView: 3,
-                    spaceBetween: 0,
-                },
-            },
-        });
-    }
-    if ($(".feature-category-slides").length > 0) {
-        new Swiper(".feature-category-slides", {
-            slidesPerView: 1,
-            spaceBetween: 0,
-            preventInteractionOnTransition: false,
-            navigation: {
-                nextEl: ".feature-category-next",
-                prevEl: ".feature-category-prev",
-            },
-        });
-    }
-    if ($("#room-space-tabs").length > 0) {
-        // Ẩn tất cả nội dung tab ngoại trừ tab đầu tiên
-        $(".tab-content").not(":first").hide(); // Ẩn tất cả các nội dung tab, ngoại trừ tab đầu tiên
+  if ($(".collection-swiper").length > 0) {
+    new Swiper(".collection-swiper", {
+      slidesPerView: 3,
+      spaceBetween: 0,
+      preventInteractionOnTransition: false,
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 0,
+        },
+      },
+    });
+  }
+  if ($(".feature-category-slides").length > 0) {
+    new Swiper(".feature-category-slides", {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      preventInteractionOnTransition: false,
+      navigation: {
+        nextEl: ".feature-category-next",
+        prevEl: ".feature-category-prev",
+      },
+    });
+  }
+  if ($("#room-space-tabs").length > 0) {
+    // Ẩn tất cả nội dung tab ngoại trừ tab đầu tiên
+    $(".tab-content").not(":first").hide(); // Ẩn tất cả các nội dung tab, ngoại trừ tab đầu tiên
 
-        // Xử lý sự kiện click trên các tab
-        $("#room-space-tabs [data-id]").on("click", function (e) {
-            e.preventDefault(); // Ngăn chặn hành vi mặc định của thẻ <a>
+    // Xử lý sự kiện click trên các tab
+    $("#room-space-tabs [data-id]").on("click", function (e) {
+      e.preventDefault(); // Ngăn chặn hành vi mặc định của thẻ <a>
 
-            var tabId = $(this).data("id"); // Lấy giá trị của thuộc tính data-id
+      var tabId = $(this).data("id"); // Lấy giá trị của thuộc tính data-id
 
-            // Ẩn tất cả các nội dung của tab
-            $(".tab-content").hide();
+      // Ẩn tất cả các nội dung của tab
+      $(".tab-content").hide();
 
-            // Hiển thị nội dung của tab tương ứng
-            $('.tab-content[data-tab-id="' + tabId + '"]').fadeIn(); // Sử dụng hiệu ứng fadeIn()
+      // Hiển thị nội dung của tab tương ứng
+      $('.tab-content[data-tab-id="' + tabId + '"]').fadeIn(); // Sử dụng hiệu ứng fadeIn()
 
-            // Loại bỏ lớp active từ tất cả các tab
-            $("#room-space-tabs [data-id]").removeClass("active");
+      // Loại bỏ lớp active từ tất cả các tab
+      $("#room-space-tabs [data-id]").removeClass("active");
 
-            // Thêm lớp active cho tab hiện tại
-            $(this).addClass("active");
-        });
-    }
+      // Thêm lớp active cho tab hiện tại
+      $(this).addClass("active");
+    });
+  }
 
-    // Room Space slide
-    if ($(".room-space-slide").length > 0) {
-        new Swiper(".room-space-slide", {
-            slidesPerView: "auto",
-            spaceBetween: 25,
-            autoplay: {
-                delay: 3000,
-            },
-            loop: true,
-            pagination: {
-                clickable: true,
-            },
-        });
-    }
-    if ($(".room-space-slide-2").length > 0) {
-        new Swiper(".room-space-slide-2", {
-            breakpoints: {
-                320: {
-                    slidesPerView: 1,
-                    spaceBetween: 0,
-                },
-                768: {
-                    slidesPerView: 3,
-                    spaceBetween: 0,
-                },
-            },
-        });
-    }
-    if ($(".best-selling-slide").length > 0) {
-        new Swiper(".best-selling-slide", {
-            scrollbar: {
-                el: ".swiper-scrollbar",
-                hide: true,
-            },
-        });
-        new Swiper(".best-selling-slide-2", {
-            scrollbar: {
-                el: ".swiper-scrollbar",
-                hide: true,
-            },
-        });
-    }
-    if ($(".partners-swiper").length > 0) {
-        new Swiper(".partners-swiper", {
-            slidesPerView: "auto",
-            autoplay: {
-                delay: 3000,
-            },
-            loop: true,
-            pagination: {
-                clickable: true,
-            },
-        });
-    }
+  // Room Space slide
+  if ($(".room-space-slide").length > 0) {
+    new Swiper(".room-space-slide", {
+      slidesPerView: "auto",
+      spaceBetween: 25,
+      autoplay: {
+        delay: 3000,
+      },
+      loop: true,
+      pagination: {
+        clickable: true,
+      },
+    });
+  }
+  if ($(".room-space-slide-2").length > 0) {
+    new Swiper(".room-space-slide-2", {
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 0,
+        },
+      },
+    });
+  }
+  if ($(".best-selling-slide").length > 0) {
+    new Swiper(".best-selling-slide", {
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+      },
+    });
+    new Swiper(".best-selling-slide-2", {
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+      },
+    });
+  }
+  if ($(".partners-swiper").length > 0) {
+    new Swiper(".partners-swiper", {
+      slidesPerView: "auto",
+      autoplay: {
+        delay: 3000,
+      },
+      loop: true,
+      pagination: {
+        clickable: true,
+      },
+    });
+  }
 
-    if ($(".trending-slides").length > 0) {
-        new Swiper(".trending-slides", {
-            slidesPerView: 1,
-            loop: true,
-        });
-    }
-    if ($(".project-slides").length > 0) {
-        new Swiper(".project-slides", {
-            breakpoints: {
-                320: {
-                    slidesPerView: 1,
-                    spaceBetween: 0,
-                },
-                768: {
-                    slidesPerView: 3,
-                    spaceBetween: 30,
-                },
-            },
-        });
-    }
+  if ($(".trending-slides").length > 0) {
+    new Swiper(".trending-slides", {
+      slidesPerView: 1,
+      loop: true,
+    });
+  }
+  if ($(".project-slides").length > 0) {
+    new Swiper(".project-slides", {
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      },
+    });
+  }
+
+  if ($(".related-articles-slides").length > 0) {
+    new Swiper(".related-articles-slides", {
+      slidesPerView: "auto",
+      spaceBetween: 25,
+      //   autoplay: {
+      //     delay: 3000,
+      //   },
+      loop: true,
+      pagination: {
+        clickable: true,
+      },
+      //   hiện thị số ảnh trong slide theo màn hình
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 0,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 2.5,
+          spaceBetween: 50,
+        },
+      },
+    });
+  }
 });
